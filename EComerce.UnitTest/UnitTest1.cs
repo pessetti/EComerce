cbc4f7038ba3215a06c119160bc773b644907de2
pessetti <pessetti17@gmail.com>
2016-06-10 16:47:39 -0300

    Primeiro commit

---
+++ EComerce.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EComerce.UnitTest
+{
+    [TestClass]
+    public class UnitTestECcomerce
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ EComerce.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EComerce.Web.MvcApplication" Language="C#" %>
